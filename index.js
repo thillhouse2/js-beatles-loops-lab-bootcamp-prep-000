785ebdf73ac3a7d65f6e44a3dda8c72acd89895d
Tim Hillhouse <thillhouse2@gmail.com>
2018-01-06 17:14:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments){
   var newArray = [];
-  for (var i = 0; i < [musicians].length; i++){
-    console.log([musicians].length)
+  for (var i = 0; i < musicians.length; i++){
+    console.log(musicians.length)
     newArray[i] = musicians[i] + " plays " + instruments[i];
     console.log([i] + ' ' + newArray[i] + ' ' + newArray.length + '\r \r');
   }
